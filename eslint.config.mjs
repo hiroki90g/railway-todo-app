ac4a9ffaa7b4b224ebe1ba2e42cb1b6f3afe7da4
林弘季 <hayashihiroki@hayashihiroshikinoMacBook-Air.local>
2025-04-07 20:28:46 +0900

    add: prettierConfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import js from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
   {
@@ -9,10 +11,16 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
     plugins: {
       react: pluginReact,
+      prettier: pluginPrettier
     },
-    extends: [js.configs.recommended, pluginReact.configs.flat.recommended],
+    extends: [
+      js.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      prettierConfig
+    ],
     rules: {
       'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error'
     },
   },
 ]);
