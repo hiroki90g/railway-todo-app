220184207dc7390e840856b3a0cf8a237c33b66e
林弘季 <hayashihiroki@hayashihiroshikinoMacBook-Air.local>
2025-04-12 04:00:46 +0900

    fix: lint rules add

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,24 @@
 import { defineConfig } from 'eslint/config';
-import globals from 'globals';
 import js from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
 import pluginPrettier from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
+import globals from 'globals';
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        test: 'readonly',
+        expect: 'readonly',
+        describe: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        process: 'readonly',
+      },
+    },
     plugins: {
       react: pluginReact,
       prettier: pluginPrettier,
@@ -21,6 +31,7 @@ export default defineConfig([
     rules: {
       'react/react-in-jsx-scope': 'off',
       'prettier/prettier': 'error',
+      'react/prop-types': 'off',
     },
   },
 ]);
